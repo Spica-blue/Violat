1c50512266b1f1cf0cca0eba90e7d110b61e6f2e
Spica-blue <heo1356@gmail.com>
2024-06-15 15:14:30 +0900

    violat에서 npm start하면 클라이언트, 서버 둘다 켜짐

--- server/member/views.py
+++ server/member/views.py
@@ -1,6 +1,10 @@
+import sys
 from pymongo import MongoClient
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+# UTF-8 인코딩을 사용하도록 설정
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 
 # Create your views here.
 
@@ -14,6 +18,6 @@ def main(request):
         collection = db["member"]
         result = collection.find({}, {'_id': 0,'id': 1, 'pwd': 1})  # 필요한 필드만 선택
         data_list = list(result)
-        print("db 접속 연결 성공 : ", data_list)
+        print("db 접속 연결 성공 : ", data_list, flush=True)
         
     return Response({'data': data_list})
